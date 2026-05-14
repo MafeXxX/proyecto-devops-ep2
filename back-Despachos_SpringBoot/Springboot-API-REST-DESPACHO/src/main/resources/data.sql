dedb6392bde9af96e9524f24a10573853148e1ff
MGuzmanL <mguzmanlanderos@gmail.com>
2026-05-13 22:32:50 -0400

    Creación data.sql
    - Se crean archivos data.sql
    - Ajustes para el correcto funcionamiento de data

---
+++ back-Despachos_SpringBoot/Springboot-API-REST-DESPACHO/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO despacho (
+    fecha_despacho,
+    patente_camion,
+    intento,
+    id_compra,
+    direccion_compra,
+    valor_compra,
+    despachado
+) VALUES
+('2026-05-14', 'ABCD12', 1, 1001, 'Av. Providencia 123', 45990, 0),
+
+('2026-05-15', 'EFGH34', 1, 1002, 'Av. Vicuña Mackenna 456', 32990, 0),
+
+('2026-05-16', 'IJKL56', 2, 1003, 'Los Leones 789', 18990, 1);
